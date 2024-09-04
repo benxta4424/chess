cd019c6f7291ea7a7eda0d7dd5eddfab3c8a7a22
benxta4424 <benxtaben@gmail.com>
2024-09-04 22:33:36 +0100

    board is a matrix of colors

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,14 @@
 require 'colorize'
+require './lib/pieces/rook'
 
 class Board
     
     def initialize
         @board=Array.new(7){Array.new(7)}
         @chess_board=[]
+
+        @first_color=:light_white
+        @second_color=:green
     end
 
     def is_even?(index_one,index_two)
@@ -14,22 +18,31 @@ class Board
     end
 
     def create_board
-
-        first_color=:light_white
-        second_color=:green
-        index_calculus=0
-
-        @board.each_with_index do |item,index|
-            item.each_with_index do |item2.index2|
-                if is_even?(index,index2)
-                    @chess_board<<"  ".colorize(background: first_color)
-                else
-                    @chess_board<<"  ".colorize(background: second_color)
-                end
-                @chess_board<<"\n"
+        @board.each_with_index do |row,row_index|
+            row.each_with_index do |col,col_index|
+                @board[row_index][col_index]=nil
             end
         end
-          
+    end
+
+    def display_board
+        @board.each_with_index do |row,row_index|
+            row.each_with_index do |col,col_index|
+                color=is_even?(row_index,col_index) ? @first_color : @second_color
+                display_color=col.nil? ? "  " : col.to_s
+                print display_color.colorize(background: color)
+            end
+            puts
+        end
+    end
+
+    def add_rooks
+        @board[0][0]=" ♖ ".colorize(background: :white)
+        @board[0][7]=" ♖ ".colorize(background: :white)
+
+        @board[7][0]=" ♜ ".colorize(background: :white)
+        @board[7][7]=" ♜ ".colorize(background: :white)
+
     end
 
 end
